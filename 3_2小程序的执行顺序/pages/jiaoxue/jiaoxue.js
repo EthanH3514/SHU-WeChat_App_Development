e82f0cfc200ebe982e19de9cc436c40b46316265
EthanH3514 <103171084+EthanH3514@users.noreply.github.com>
2022-10-02 21:40:25 +0800

    Add files via upload

---
+++ 3_2小程序的执行顺序/pages/jiaoxue/jiaoxue.js
@@ -0,0 +1,18 @@
+// pages/jiaoxue/jiaoxue.js
+Page({
+    onLoad:function(options){
+        console.log('--jiaoxue.js--onLoad--页面加载')
+    },
+    onReady:function(){
+        console.log('--jiaoxue.js--onReady--页面初次渲染完成')
+    },
+    onShow:function(){
+        console.log('--jiaoxue.js--onShow--页面显示')
+    },
+    onHide:function(){
+        console.log('--jiaoxue.js--onHide--页面隐藏')
+    },
+    onUnload:function(){
+        console.log('--jiaoxue.js--onUnload--页面卸载')
+    }
+})
